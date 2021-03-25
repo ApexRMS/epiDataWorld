e312aeb094dc7cbc3ea065335e70d93a9b1ce096
vlucet <VLucet@users.noreply.github.com>
2021-03-25 20:05:32 +0000

    get data from inputs

--- 1_download_data.R
+++ 1_download_data.R
@@ -3,8 +3,37 @@
 # WIP
 
 library(rsyncrosim)
+library(COVID19)
+
 myLib <- ssimLibrary()
 mySce <- scenario()
+e <- ssimEnvironment()
+
+source(file.path(e$PackageDirectory, "epiDataWorld_functions.R"))
 
 # Get Input datasheets
 
+inputs <- datasheet(mySce, "epiDataWorld_Inputs", lookupsAsFactors = FALSE)
+
+if (length(inputs$Jurisdiction) != 0){
+  
+  juris <- inputs$Jurisdiction
+  
+  if (juris == "World"){
+    juris <- NULL
+  }
+  
+} else {
+  stop("No Jurisdiction provided")
+}
+
+if(length(inputs$Level) != 0){
+  level <- lookup_level(inputs$Level)
+} else {
+  message("No level provided, devfault to level 1 (Country)")
+}
+
+# Get data
+
+covidData <- COVID19::covid19(country = juris, level = level, 
+                              verbose = FALSE)
