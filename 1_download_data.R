99c824903aa10aebf6ead8367db75ca150d5b21b
vlucet <VLucet@users.noreply.github.com>
2021-03-25 21:00:46 +0000

    progress in rocessing data

--- 1_download_data.R
+++ 1_download_data.R
@@ -2,38 +2,131 @@
 # Using package COVID19
 # WIP
 
+# Load packages -----------------------------------------------------------
+
 library(rsyncrosim)
 library(COVID19)
+library(dplyr)
+library(tidyr)
+
+# Load Vars ---------------------------------------------------------------
 
 myLib <- ssimLibrary()
 mySce <- scenario()
 e <- ssimEnvironment()
+transferDir <- e$TransferDirectory
+
+# Source helpers ----------------------------------------------------------
 
 source(file.path(e$PackageDirectory, "epiDataWorld_functions.R"))
 
-# Get Input datasheets
+# Define variables (now harcoded) -----------------------------------------
+
+vars <- c("Cases - Daily", "Cases - Cumulative")
+
+# Query inputs ------------------------------------------------------------
 
 inputs <- datasheet(mySce, "epiDataWorld_Inputs", lookupsAsFactors = FALSE)
 
+# Check jurisdiction
 if (length(inputs$Jurisdiction) != 0){
   
-  juris <- inputs$Jurisdiction
+  juris_input <- inputs$Jurisdiction
   
   if (juris == "World"){
-    juris <- NULL
+    
+    juris_covid <- NULL
+    
+  } else {
+    
+    juris_covid <- juris_input
+    
   }
   
 } else {
+  
   stop("No Jurisdiction provided")
+  
 }
 
+# Check level
 if(length(inputs$Level) != 0){
-  level <- lookup_level(inputs$Level)
+  
+  level_input <- inputs$Level
+  
+  level_covid <- lookup_level(inputs$Level)
+  
+  # Make admin level name
+  admin_level_name <- paste0("administrative_area_level_", level_covid)
+  
 } else {
+  
   message("No level provided, devfault to level 1 (Country)")
+  
 }
 
-# Get data
+# TODO use this to filter data
+runControl <- datasheet(mySce, "epi_RunControl")
 
-covidData <- COVID19::covid19(country = juris, level = level, 
+# Download data -----------------------------------------------------------
+
+covidData <- COVID19::covid19(country = juris_covid, 
+                              level = level_covid, 
                               verbose = FALSE)
+covidDataSubset <- covidData %>% ungroup() %>% 
+  select(date, confirmed, Jurisdiction = all_of(admin_level_name)) %>% 
+  group_by(Jurisdiction) #%>% 
+#arrange(date, descending = TRUE)
+
+# Save in epi package -----------------------------------------------------
+
+# Get the vector of jurisdisctions
+allJuris <- unique(covidDataSubset$Jurisdiction)
+
+# Add the required variables and jurisdictions to the SyncroSim project
+saveDatasheet(mySce, 
+              data.frame(Name = allJuris), "epi_Jurisdiction")
+saveDatasheet(mySce, 
+              data.frame(Name = vars), "epi_Variable")
+
+
+# Process Data ------------------------------------------------------------
+
+covidDataFinal <- data.frame()
+
+if ("Cases - Daily" %in% vars){
+  
+  covidDataFinal <- covidDataFinal %>% 
+    bind_rows(
+      covidDataSubset %>% 
+        rename(Timestep = date, value = confirmed) %>% 
+        mutate(Variable = "Cases - Daily") %>% 
+        ungroup()
+    )
+  
+}
+
+if ("Cases - Cumulative" %in% vars){
+  
+  covidDataFinal <- covidDataFinal %>% 
+    bind_rows(
+      covidDataSubset %>% 
+        rename(Timestep = date, value = confirmed) %>% 
+        arrange(Timestep) %>% 
+        mutate(value = cumsum(ifelse(is.na(value), 0, value))) %>% 
+        mutate(Variable = "Cases - Cumulative") %>% 
+        ungroup()
+      
+    )
+
+}
+
+# Write out data ----------------------------------------------------------
+
+juris_no_space <- gsub("[[:space:]]", "_", juris_input)
+
+fileName <- paste0("COVID19_Data_", juris_no_space, "_by_", level_input, ".csv")
+
+filePath <- file.path(transferDir, fileName)
+
+write.csv(covidDataFinal, filePath, row.names = FALSE)
