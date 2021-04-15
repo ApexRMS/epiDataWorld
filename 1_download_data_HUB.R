b455ce7190216cde5f3a08630d1393127ca0a3e4
vlucet <VLucet@users.noreply.github.com>
2021-04-15 18:00:37 +0000

    add numbering

--- 1_download_data_HUB.R
+++ 1_download_data_HUB.R
@@ -12,27 +12,27 @@ source(file.path(E$PackageDirectory, "0_epiDataWorld_functions.R"))
 source(file.path(E$PackageDirectory, "0_CovidHUB_functions.R"))
 source(file.path(E$PackageDirectory, "0_JHU_functions.R"))
 
-# Load data
+# 1. Load data
 
 inputs <- load_inputs(backend = "HUB", mySce = SCE, e = E)
 
-# Save to epi
+# 2. Save to epi
 
 save_to_epi(dataSubset = inputs$covidDataSubset, mySce = SCE, vars = VARS)
 
-# Process data and save it
+# 3. Process data and save it
 
 covidDataFinal <- process_data(vars = VARS, covidDataSubset = inputs$covidDataSubset) %>% 
   mutate(TransformerID = TRANSFORMER_NAME)
 saveDatasheet(SCE, covidDataFinal, "epi_DataSummary")
 
-# Write out data
+# 4. Write out data
 
 fileName <- make_filename(inputs_vars = inputs$input_vars)
 filePath <- file.path(E$TransferDirectory, fileName)
 
 write.csv(covidDataFinal, filePath, row.names = FALSE)
 
-# Save outpout
+# 5. Save outpout
 
 save_output_info(mySce = SCE, input_vars = inputs$input_vars, backend = "HUB", filePath = filePath)
