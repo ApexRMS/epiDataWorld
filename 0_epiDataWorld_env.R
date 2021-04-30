4d55b7ac4caa1b5b4d76562ab44ff89ced787bed
vlucet <VLucet@users.noreply.github.com>
2021-04-30 19:15:11 +0000

    aadd lookup

--- 0_epiDataWorld_env.R
+++ 0_epiDataWorld_env.R
@@ -17,4 +17,15 @@ VARS <- c("Cases - Cumulative", "Cases - Daily",
           "Active - Daily", # Active cases are daily by default
           "Recovered - Cumulative", "Recovered - Daily", 
           "Tested - Cumulative", "Tested - Daily",
-          "Deaths - Cumulative", "Deaths - Daily")
+          "Deaths - Cumulative", "Deaths - Daily", 
+          "Vaccines - Cumulative", "Vaccines - Daily")
+RAWVARS_HUB <- c("confirmed", "dailyconfirmed", 
+                 NA, 
+                 "recovered", "dailyrecovered", 
+                 "tested", "dailytested", 
+                 "deaths", "dailydeaths", 
+                 "vaccines", "dailyvaccines")
+RAWVARS_JHU <- NA
+LOOKUP <- data.frame(VARS = VARS,
+                     RAWVARS_HUB = RAWVARS_HUB, 
+                     RAWVARS_JHU = RAWVARS_JHU)
