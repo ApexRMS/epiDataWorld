3dff4bfbbc0e689d2c0af1107bb9c22ee5d05d5c
vlucet <VLucet@users.noreply.github.com>
2021-04-30 18:35:46 +0000

    load more vars

--- 0_epiDataWorld_env.R
+++ 0_epiDataWorld_env.R
@@ -13,4 +13,8 @@ SCE <- scenario()
 
 # TRANSFER_DIR <- e$TransferDirectory
 
-VARS <- c("Cases - Daily", "Cases - Cumulative")
+VARS <- c("Cases - Cumulative", "Cases - Daily", 
+          "Active - Daily", # Active cases are daily by default
+          "Recovered - Cumulative", "Recovered - Daily", 
+          "Tested - Cumulative", "Tested - Daily",
+          "Deaths - Cumulative", "Deaths - Daily")
