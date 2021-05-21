47fc148ac3f78ac37095d4b6ba85bd17255d9945
Valentin <valentin.lucet@gmail.com>
2021-05-21 14:25:35 -0400

    add barebone script

---
+++ 1_download_data_OWD.R
@@ -0,0 +1,38 @@
+# JHU ---------------------------------------------------------------------
+
+library(rsyncrosim)
+E <- ssimEnvironment()
+TRANSFORMER_NAME <- "Download from Our World in Data"
+
+# Source helpers ----------------------------------------------------------
+
+source(file.path(E$PackageDirectory, "0_epiDataWorld_env.R"))
+source(file.path(E$PackageDirectory, "0_epiDataWorld_functions.R"))
+source(file.path(E$PackageDirectory, "0_OWD_functions.R"))
+
+# 1. Load data
+
+inputs <- load_inputs(backend = "OWD", mySce = SCE, e = E)
+
+# 2. Save to epi
+
+save_to_epi(dataSubset = inputs$covidDataSubset, mySce = SCE, vars = VARS)
+
+# 3. Process data and save it
+
+covidDataFinal <- process_data(covidDataSubset = inputs$covidDataSubset, 
+                               lookup = LOOKUP) %>% 
+  mutate(TransformerID = TRANSFORMER_NAME) %>% 
+  replace_na(list(Value = 0))
+saveDatasheet(SCE, covidDataFinal, "epi_DataSummary", append = TRUE)
+
+# 4. Write out data
+
+fileName <- make_filename(inputs_vars = inputs$input_vars, backend = "OWD")
+filePath <- file.path(E$TransferDirectory, fileName)
+
+write.csv(inputs$raw, filePath, row.names = FALSE)
+
+# 5. Save outpout
+
+save_output_info(mySce = SCE, input_vars = inputs$input_vars, backend = "OWD", filePath = filePath)
