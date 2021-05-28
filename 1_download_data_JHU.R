09423566e2175eb2e35650a6f8bd6dca60385ad8
vlucet <VLucet@users.noreply.github.com>
2021-05-28 16:45:50 -0400

    remove NA records

--- 1_download_data_JHU.R
+++ 1_download_data_JHU.R
@@ -22,8 +22,8 @@ save_to_epi(dataSubset = inputs$covidDataSubset, mySce = SCE, vars = VARS)
 
 covidDataFinal <- process_data(covidDataSubset = inputs$covidDataSubset, 
                                lookup = LOOKUP) %>% 
-  mutate(TransformerID = TRANSFORMER_NAME) %>% 
-  replace_na(list(Value = 0))
+  mutate(TransformerID = TRANSFORMER_NAME) %>%
+  dplyr::filter(!is.na(Value))
 saveDatasheet(SCE, covidDataFinal, "epi_DataSummary", append = TRUE)
 
 # 4. Write out data
